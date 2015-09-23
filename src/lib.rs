853d8066624cecc93e815c5784d16826a699d0c6
benaryorg <binary@benary.org>
2015-09-23 13:17:10 +0200

    documentation and inline tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,34 @@
+//! This crate provides only one struct, `PrimeIter` implementing `Iterator`
+//! which yields prime numbers as an `u64`.
+//!
+
+/// `PrimeIter` is used for generating an endless stream of prime numbers in an
+/// efficient way.
+///
+/// The implementation of `Iterator` makes it easy to iterate over prime
+/// numbers.
+///
+/// Generating the first ten prime numbers:
+///
+/// ```
+/// # use primeiter::PrimeIter;
+///
+/// let primes = PrimeIter::new();
+/// let v: Vec<_> = primes.take(10).collect();
+///
+/// assert_eq!(v,vec![2,3,5,7,11,13,17,19,23,29]);
+/// ```
+///
+/// Printing an endless stream of prime numbers:
+///
+/// ```no_run
+/// # use primeiter::PrimeIter;
+/// for prime in PrimeIter::new()
+/// {
+///		println!("{}",prime);
+/// }
+/// ```
+///
 pub struct PrimeIter
 {
 	primes: Vec<u64>,
@@ -5,6 +36,15 @@ pub struct PrimeIter
 
 impl PrimeIter
 {
+	/// Returns a new instance of `PrimeIter`.
+	///
+	/// # Examples
+	///
+	/// ```
+	/// # use primeiter::PrimeIter;
+	/// let iter = PrimeIter::new();
+	/// ```
+	///
 	pub fn new()->PrimeIter
 	{
 		PrimeIter
@@ -36,19 +76,5 @@ impl Iterator for PrimeIter
 		}
 		None
 	}
-}
-
-#[test]
-fn creation()
-{
-	let _iter = PrimeIter::new();
-}
-
-#[test]
-fn first_primes()
-{
-	let primes = PrimeIter::new();
-	let v: Vec<_> = primes.take(10).collect();
-	assert_eq!(v,vec![2,3,5,7,11,13,17,19,23,29]);
 }
 
